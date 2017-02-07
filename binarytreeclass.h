0a2c75c89c0652020f6afd060f9bfefb06128795
kotlavaibhav <kotlavaibhav@gmail.com>
2017-02-07 11:14:12 -0800

    Create binarytreeclass.h

---
+++ binarytreeclass.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+
+class bst{
+bst();
+void insert(int item);
+void deleteitem(int item);
+void deletefromtree(node* &p);
+void levelordertraversal(node *current);
+void lot();
+node* leastcommonancestor(node *current,int A,int B);
+void lothelp(int A,int B);
+deleteall(node *p);
+void deleteallleaves(node * p);
+int count1(node* p);
+void counthelp();
+deleteallleaves1();
+};
+
+
+
+	
